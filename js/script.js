3fdb7dafc84e0806ba04e496899459c78c25dcc1
Elvis Maliza <emaliza2482@gmail.com>
2018-11-15 23:07:44 +0000

    Day 2 Edits

--- js/script.js
+++ js/script.js
@@ -11,3 +11,29 @@
 $("#shoot").click(function(){
     $("#userChoice").text($("#input").val());
 });
+
+var userChoice= " " ;
+var computerChoice= " " ;
+var winner= " " ;
+var randomNumber= 0;
+
+$("#shoot").click(function(){
+    var userChoice= $("#input").val();
+    $("#userChoice").text(userChoice);
+    
+    var randomNumber= Math.random()*3.0;
+    console.log(randomNumber);
+    if ( randomNumber < 1){
+    $("#computerChoice").text("rock");
+    }
+    else if (randomNumber < 2){
+        $("#computerChoice").text("scissors");
+    }
+    else{
+        $("#computerChoice").text("paper");
+    }
+    
+
+});
+
+
